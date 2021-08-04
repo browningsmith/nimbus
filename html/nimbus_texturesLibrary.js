29d108c3c9252dc4ec9a40a11de4d16317ba6bce
Keith Smith <browningsmith@email.arizona.edu>
2021-08-03 20:57:46 -0700

    Added new textures this time clicking away from camera rotations each time

--- html/nimbus_texturesLibrary.js
+++ html/nimbus_texturesLibrary.js
@@ -10,37 +10,37 @@ let textures = {
 
     negZplane: {
 
-        url: "http://www.keith-smith.rocks/nimbus_textures/firstCloud4_nz.png",
+        url: "http://www.keith-smith.rocks/nimbus_textures/firstCloud1_nz.png",
         texture: null,
     },
 
     posXplane: {
 
-        url: "https://www.keith-smith.rocks/nimbus_textures/firstCloud4_px.png",
+        url: "https://www.keith-smith.rocks/nimbus_textures/firstCloud1_px.png",
         texture: null,
     },
 
     posZplane: {
 
-        url: "https://www.keith-smith.rocks/nimbus_textures/firstCloud4_pz.png",
+        url: "https://www.keith-smith.rocks/nimbus_textures/firstCloud1_pz.png",
         texture: null,
     },
 
     negXplane: {
 
-        url: "https://www.keith-smith.rocks/nimbus_textures/firstCloud4_nx.png",
+        url: "https://www.keith-smith.rocks/nimbus_textures/firstCloud1_nx.png",
         texture: null,
     },
 
     posYplane: {
 
-        url: "https://www.keith-smith.rocks/nimbus_textures/firstCloud4_py.png",
+        url: "https://www.keith-smith.rocks/nimbus_textures/firstCloud1_py.png",
         texture: null,
     },
 
     negYplane: {
 
-        url: "https://www.keith-smith.rocks/nimbus_textures/firstCloud4_ny.png",
+        url: "https://www.keith-smith.rocks/nimbus_textures/firstCloud1_ny.png",
         texture: null,
     },
 };
