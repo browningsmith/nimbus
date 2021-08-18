3cc23361806505af3f3ca8527b921896969ca6e5
Keith Smith <browningsmith@email.arizona.edu>
2021-08-17 20:23:44 -0700

    More testing of accessing data from textures

--- volumetrics/volumetrics.js
+++ volumetrics/volumetrics.js
@@ -15,6 +15,8 @@ let shaderData = {
 
     fragmentShaderCode: `
     
+        #define PI 3.1415926538
+    
         precision highp float;
     
         uniform vec2 u_resolution;
@@ -26,33 +28,8 @@ let shaderData = {
         {
             vec2 st = gl_FragCoord.xy/u_resolution;
             vec3 color = vec3(0.0);
-
-            // Get texture data for this pixel
-            vec4 texData = texture2D(u_sampler, st);
-
-            float compressed_dimension = 2.0; // Size of one side of the texture sampler, as loaded
-
-            vec2 st_expanded = st * compressed_dimension;
-            vec2 st_expanded_f = fract(st_expanded);
-
-            if ((st_expanded_f.x < 0.5) && (st_expanded_f.y < 0.5))
-            {
-                color = vec3(texData.x);
-            }
-            else if (st_expanded_f.y < 0.5)
-            {
-                color = vec3(texData.y);
-            }
-            else if (st_expanded_f.x < 0.5)
-            {
-                color = vec3(texData.z);
-            }
-            else
-            {
-                color = vec3(texData.w);
-            }
         
-            gl_FragColor = vec4(color.x, 0.0, color.x, 1.0);
+            gl_FragColor = texture2D(u_sampler, st);
         }
     `,
 
